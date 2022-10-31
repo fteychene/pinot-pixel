77ebfc81b1b54cae39a73d9fb81b2d8aee7b8086
fteychene <francois.teychene@gmail.com>
2022-10-31 10:21:55 +0100

    feat: Add old data for hybrid data to avoid complx setup for peoples

--- pinot/hybrid_data/import_hybrid_closer.sh
+++ pinot/hybrid_data/import_hybrid_closer.sh
@@ -4,8 +4,8 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 for i in {00..43}
 do
-    echo "Import pixel_updated_$i.json"
-    curl -X POST -F file=@${script_dir}/pixel_updated_$i.json \
+    echo "Import pixel_updated_closer_$i.json"
+    curl -X POST -F file=@${script_dir}/pixel_updated_closer_$i.json \
               -H "Content-Type: multipart/form-data" \
               "http://localhost:9000/ingestFromFile?tableNameWithType=pixelEvent_OFFLINE&batchConfigMapStr=%7B%22inputFormat%22%3A%22json%22%7D"
     echo
